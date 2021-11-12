4a0fd206ec78f02270dad6605a2ebd9a2482a0d9
Tali04001 <89461076+Tali04001@users.noreply.github.com>
2021-11-12 15:02:38 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace sdfghjkl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n,r,s=0,temp; //eftersom de har samma värde slipper jag göra flera int.
+            Console.WriteLine("skriv ett 2siffrigt tal som minst:");
+            n = int.Parse(Console.ReadLine());
+            temp = n;
+            while(n>0){
+
+                r=n%10;
+                s=(s*10)+r;
+                n=n/10;
+            }
+            if(temp==s)
+             Console.WriteLine("ja det är ett palindrom");
+
+            else
+            Console.WriteLine("Nej det är inte ett palindrom");
+
+        }
+    }
+}
